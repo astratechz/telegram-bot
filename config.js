5d643b82a6306fd4c1c0e68f465e78f8d2150d05
astratechz <esimtempo@gmail.com>
2026-03-26 13:07:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -10,12 +10,12 @@ const config = {
   owner_id: process.env.OWNER_ID || "", // Your telegram chat id
   prefix: process.env.PREFIX || "/", // Your preferred prefix
   apiKey: process.env.API_KEY || "gifted", //Replace with your unlimited/paid apikey
-  botName: process.env.BOT_NAME || "GIFTED-MD",
+  botName: process.env.BOT_NAME || "ASTRA-MD",
   timezone: process.env.TIMEZONE || "Africa/Nairobi",
   ownerName: process.env.OWNER_NAME || "Maurice Gift",
-  ownerUsername: process.env.OWNER_USERNAME || "mauricegift",
+  ownerUsername: process.env.OWNER_USERNAME || "astra3code",
   apiUrl: process.env.API_URL || "https://api.giftedtech.co.ke", // Can replce with yours
-  artistName: process.env.ARTIST_NAME || "Powered by Gifted Apis",
+  artistName: process.env.ARTIST_NAME || "Powered by AstraTech Apis",
   url: process.env.URL || "https://gitcdn.giftedtech.co.ke/image/AZO_image.jpg",
   sourceUrl: process.env.SOURCE_URL || "https://github.com/mauricegift/telegram-bot",
 
